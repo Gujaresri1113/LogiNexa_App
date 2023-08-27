cdab400adefb3cbffec7ee722ef2df5cd3ee9ed3
gujaresri1113 <srikrishnagujare123@gmail.com>
2023-08-27 13:33:39 +0530

    config

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "YQ0ddWpaa7SfJTFjSepO67MSFTdQUWGwt+X08KMg3iE=",
+    EMAIL : "floyd.will56@ethereal.email",
+    PASSWORD : "Ver7fuQZA48A5yqqaq",
+    ATLAS_URI: "mongodb+srv://srikrishnagujare123:Admin123@cluster0.iehgrgi.mongodb.net/?retryWrites=true&w=majority"
+}
