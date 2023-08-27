d284b14f53d2952a0e6e458e5588669c49007875
Gujaresri1113 <123804722+Gujaresri1113@users.noreply.github.com>
2023-08-27 13:48:48 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export default {
     JWT_SECRET : "YQ0ddWpaa7SfJTFjSepO67MSFTdQUWGwt+X08KMg3iE=",
-    EMAIL : "floyd.will56@ethereal.email",
-    PASSWORD : "Ver7fuQZA48A5yqqaq",
+    EMAIL : "adela3@ethereal.email",
+    PASSWORD : "FTjAtYwCRwayu8T8pt",
     ATLAS_URI: "mongodb+srv://srikrishnagujare123:Admin123@cluster0.iehgrgi.mongodb.net/?retryWrites=true&w=majority"
 }
